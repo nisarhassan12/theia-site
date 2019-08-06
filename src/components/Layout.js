3464758f97d0cfbed914a44fe07f5dc417ae414d
nisarhassan12 <syednisarhassan12@gmail.com>
2019-08-06 05:49:05 +0000

    Adds Favicon.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import { Global, css } from '@emotion/core'
 import Helmet from 'react-helmet'
 import useSiteMetadata from '../hooks/use-site-metadata'
 import { fontSizes, colors, grid, breakpoints } from '../utils/variables'
+import Favicon from '../resources/favicon.ico'
 
 const Layout = ({ children }) => {
     const { title, description } = useSiteMetadata()
@@ -105,24 +106,7 @@ const Layout = ({ children }) => {
 	            <link rel="canonical" href="https://www.theia-ide.org/" />
 	            <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
-                <link rel="shortcut icon" href={require("../resources/favicon.ico")} />
-                <link rel="apple-touch-icon" href={require("../resources/apple-touch-icon.png")} />
-                <link rel="apple-touch-icon" sizes="57x57" href={require("../resources/apple-icon-57x57.png")} />
-                <link rel="apple-touch-icon" sizes="60x60" href={require("../resources/apple-icon-60x60.png")} />
-                <link rel="apple-touch-icon" sizes="72x72" href={require("../resources/apple-icon-72x72.png")} />
-                <link rel="apple-touch-icon" sizes="76x76" href={require("../resources/apple-icon-76x76.png")} />
-                <link rel="apple-touch-icon" sizes="114x114" href={require("../resources/apple-icon-114x114.png")} />
-                <link rel="apple-touch-icon" sizes="120x120" href={require("../resources/apple-icon-120x120.png")} />
-                <link rel="apple-touch-icon" sizes="144x144" href={require("../resources/apple-icon-144x144.png")} />
-                <link rel="apple-touch-icon" sizes="152x152" href={require("../resources/apple-icon-152x152.png")} />
-                <link rel="apple-touch-icon" sizes="180x180" href={require("../resources/apple-icon-180x180.png")} />
-                <link rel="icon" type="image/png" sizes="192x192" href={require("../resources/android-icon-192x192.png")} />
-                <link rel="icon" type="image/png" sizes="32x32" href={require("../resources/favicon-32x32.png")} />
-                <link rel="icon" type="image/png" sizes="96x96" href={require("../resources/favicon-96x96.png")} />
-                <link rel="icon" type="image/png" sizes="16x16" href={require("../resources/favicon-16x16.png")} />
-                <link rel="manifest" href={require("../resources/manifest.json")} />
-                <meta name="msapplication-TileColor" content="#ffffff" />
-                <meta name="msapplication-TileImage" content={require("../resources/ms-icon-144x144.png")} />
+                <link rel="shortcut icon" href={Favicon} />
             </Helmet>
             <>
                 {children}
